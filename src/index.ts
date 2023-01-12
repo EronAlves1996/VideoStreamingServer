a5f7b24bd5794f2aee05509c6c085de162c183cc
Eron Alves <eron.a.silva@capgemini.com>
2023-01-12 12:21:28 -0300

    feat: create send file util

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
-import Koa from "koa";
+import Koa, { ParameterizedContext } from "koa";
+import Router from "koa-router";
+import send from "koa-send";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = parseInt(process.env.PORT!, 10) || 3000;
 const app = new Koa();
+const router = new Router();
+
+router.get("/", async (ctx: Koa.Context) => {
+  await send(ctx, path.resolve(__dirname, "public", "index.html"));
+
+  if (!ctx.status) {
+    ctx.throw(404);
+  }
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(PORT);
 console.log("Video Streaming Server is running on Port", PORT);
