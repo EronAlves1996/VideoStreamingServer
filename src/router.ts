d5a6c332dceae3ee4ab114c9c4255ced6cfcf20a
Eron Alves <eron.a.silva@capgemini.com>
2023-01-12 14:18:29 -0300

    feat: create th elogic to send video

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { access } from "fs";
+import { access, createReadStream, stat } from "fs";
 import { Context, Middleware } from "koa";
 import Router from "koa-router";
 import send from "koa-send";
@@ -37,6 +37,28 @@ router.get("/api/video/:name", async (ctx: Context, next: Middleware) => {
 
   const parts = range.replace("bytes=", "").split("-");
   const rangeStart = parts[0] && parts[0].trim();
+  const start = rangeStart ? parseInt(rangeStart, 10) : 0;
+
+  const videoStat = await promisify(stat)(videoPath);
+  const videoSize = videoStat.size;
+  const chunkSize = 10 ** 6;
+
+  const rangeEnd = parts[1] && parts[1].trim();
+  const __rangeEnd = rangeEnd ? parseInt(rangeEnd, 10) : undefined;
+  const end =
+    __rangeEnd === 1 ? __rangeEnd : Math.min(start + chunkSize, videoSize) - 1;
+  const contentLength = end - start + 1;
+
+  response.set("Content-Range", `bytes ${start}-${end}/${videoSize}`);
+  response.set("Accept-Ranges", "bytes");
+  response.set("Content-Length", contentLength.toString());
+
+  const stream = createReadStream(videoPath, { start, end });
+  stream.on("error", (err) => console.log(err.toString()));
+
+  response.status = 206;
+  response.type = path.extname(name);
+  response.body = stream;
 });
 
 export default router;
